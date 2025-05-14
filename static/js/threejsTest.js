a26f181d2924ee9040b970ac535811a3bfaff7e3
田中喜作 <39n1301@gmail.com>
2025-05-14 15:27:03 +0900

    add light

--- static/js/threejsTest.js
+++ static/js/threejsTest.js
@@ -23,7 +23,17 @@ init()
 animate()
 
 function init(){
-    
+    geometry = new THREE.BoxGeometry(250,250,250);
+    material = new THREE.MeshBasicMaterial({
+        color:0xffffff
+    });
+    box = new THREE.Mesh(geometry,material);
+    scene.add(box);
+
+    light = new THREE.DirectionalLight(0xffffff);
+    light.intensity = 2;
+    light.position.set(1,1,1);
+    scene.add(light);
 }
 
 function animate(){
